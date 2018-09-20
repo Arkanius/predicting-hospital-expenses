a682c61f3b6814912484340229a8695c586de6ab
Arkanius <vtr.gomes@convenia.com.br>
2018-09-20 00:04:23 -0300

    model adjustments

--- predicting-hospital-expenses.R
+++ predicting-hospital-expenses.R
@@ -1,6 +1,9 @@
 setwd("~/Documents/dsa/RFundamentos/miniprojeto3")
 getwd()
 
+install.packages("ggplot2")
+install.packages("ggthemes")
+install.packages("dplyr")
 
 ## Etapa 1 - Coletando os Dados
 data <- read.csv("./despesas.csv", stringsAsFactors = TRUE)
@@ -50,7 +53,38 @@ head(prevision)
 
 res <-residuals(model)
 class(res)
+res <- as.data.frame(res)
+head(res)
+
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+
+ggplot(res, aes(res)) +  
+  geom_histogram(fill = 'blue', 
+                 alpha = 0.5, 
+                 binwidth = 1)
+
+?cbind
+result <- cbind(prevision, testData$gastos) 
+colnames(result) <- c('predicted','real')
+class(result)
+result <- as.data.frame(result)
+head(result)
+min(result)
+class(result)
 
 
 # MSE 
-#mse <- mean(())
+mse <- mean((result$real - result$predicted)^2)
+mse
+
+# RMSE 
+rmse <- mse ^ 0.5
+
+# R Squared
+sse = sum((result$predicted - result$real)^2)
+sst = sum((mean(data$gastos) - result$real)^2)
+rSquared = 1 - (sse/sst)
+
+rSquared
